9b1698080b7dfa6bf598a5154c340b0aabbd539b
antony <tehanovanton@gmail.com>
2022-02-10 22:55:48 +0300

    Publish app and test httpHandler

---
+++ IISHandler1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Web;
+
+namespace lab1_a
+{
+    public class IISHandler1 : IHttpHandler
+    {
+        /// <summary>
+        /// Вам потребуется настроить этот обработчик в файле Web.config вашего 
+        /// веб-сайта и зарегистрировать его с помощью IIS, чтобы затем воспользоваться им.
+        /// см. на этой странице: https://go.microsoft.com/?linkid=8101007
+        /// </summary>
+        
+        public bool IsReusable
+        {            
+            get { return true; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            HttpResponse res = context.Response;
+            res.Write("IISHandler1");
+        }
+    }
+}
