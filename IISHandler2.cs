ddc66504355eb56b21cea1e55e31a0ffbc376551
antony <tehanovanton@gmail.com>
2022-02-11 19:27:46 +0300

    Task2 done

--- IISHandler2.cs
+++ IISHandler2.cs
@@ -1,31 +1,33 @@
 ﻿using System;
-using System.Web;
 using System.Collections.Specialized;
+using System.Web;
+using System.IO;
+
 
 namespace lab1_a
 {
-    public class IISHandler1 : IHttpHandler
+    public class IISHandler2 : IHttpHandler
     {        
         public bool IsReusable
         {            
             get { return true; }
         }
 
-        public NameValueCollection QueryString(HttpRequest request)
+        public NameValueCollection Params(HttpRequest request)
         {
-            return request.QueryString;
+            return request.Params;
         }
-         
+
         public void ProcessRequest(HttpContext context)
         {
             HttpResponse res = context.Response;
             HttpRequest req = context.Request;
 
-            NameValueCollection queryString = QueryString(req);
-            String response = $"Get-Http-TAV:ParmA = {queryString["a"]}, ParmB = {queryString["b"]}";
+            NameValueCollection parameters = Params(req);
+            string response = $"Post-Http-TAV:ParmA = {parameters["a"]}, ParmB = {parameters["b"]}";
 
             res.ContentType = "text/plain";
             res.Write(response);
-        }
+        }        
     }
 }
